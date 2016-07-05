fbed540d9efad2527dd5e8489464a986402b2e9f
Elif Koc <elifkoc@nyu.edu>
2016-07-05 14:46:21 -0700

    Add athlete boxes

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,19 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
-
+var dot = require("./lib/dot");
+var template = dot.compile(require("./_athletebox.html"));
 
 var qsa = require("./lib/qsa");
 
 var onClick = function(e) {
  
   var pos = this.getAttribute("data-id");
-  var item = document.querySelector(`.athlete-info[data-id="${pos}"]`);
-  item.classList.toggle('show');
+  var item = document.querySelector(`.athlete-box`);
+  
+  item.classList.add("show");
+  item.innerHTML = template({athletes: window.olympians, id: pos});
+  
   
 }
 
