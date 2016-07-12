5b3799ef9d52fba78dbf05da435901a23c6fd009
Elif Koc <elifkoc@nyu.edu>
2016-07-12 15:05:16 -0700

    Athlete boxes

--- src/js/main.js
+++ src/js/main.js
@@ -8,18 +8,22 @@ var template = dot.compile(require("./_athletebox.html"));
 
 var qsa = require("./lib/qsa");
 
+var box = qsa(".athletebox");
+
 var onClick = function(e) {
- 
+
+  console.log();
   var pos = this.getAttribute("data-id");
-  var item = document.querySelector(`.athlete-box`);
-  
+  var item = document.querySelector(`.athlete-box[data-id="${pos}"]`);
+
   item.classList.add("show");
   item.innerHTML = template({athletes: window.olympians, id: pos});
-  
-  
+
+
 }
 
 
 qsa(".athlete-name").forEach(function(el) {
+
   el.addEventListener("click", onClick);
 } );
