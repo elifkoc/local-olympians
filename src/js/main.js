52cd5e912cc696a036073f7db9f971257c7efde0
Elif Koc <elifkoc@nyu.edu>
2016-07-12 17:39:19 -0700

    Highlighting columns

--- src/js/main.js
+++ src/js/main.js
@@ -12,18 +12,43 @@ var box = qsa(".athletebox");
 
 var onClick = function(e) {
 
-  console.log();
   var pos = this.getAttribute("data-id");
   var item = document.querySelector(`.athlete-box[data-id="${pos}"]`);
 
-  item.classList.add("show");
-  item.innerHTML = template({athletes: window.olympians, id: pos});
+  if (!item.classList.contains("show")) {
+    item.classList.add("show");
+    item.innerHTML = template({athletes: window.olympians, id: pos});
+  }
 
-
+  else {
+    item.classList.remove("show");
+  }
 }
 
 
-qsa(".athlete-name").forEach(function(el) {
+var colHover = function(e) {
+  var pos = this.getAttribute("data-id");
+  var items = qsa(`.row-item[data-id="${pos}"]`);
+    console.log(items);
+  var others = qsa('.row-item');
+  
+  others.forEach(function(i) {
+    i.classList.remove("highlight");
 
+  });
+                 
+  
+  items.forEach(function(i) {
+      i.classList.add("highlight");
+
+  });
+}
+
+qsa(".athlete-name").forEach(function(el) {
   el.addEventListener("click", onClick);
 } );
+
+qsa(".row-item").forEach(function(el) {
+  el.addEventListener("mouseover", colHover);
+} );
+
