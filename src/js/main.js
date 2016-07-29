caae11e4159a9f9137ac371ebeadd081d4d69668
Elif Koc <elifkoc@nyu.edu>
2016-07-29 15:50:07 -0700

    Final fixes

--- src/js/main.js
+++ src/js/main.js
@@ -4,29 +4,75 @@
 
 require("component-responsive-frame/child");
 var dot = require("./lib/dot");
+var animate = require("./lib/animateScroll");
 var template = dot.compile(require("./_athletebox.html"));
 var qsa = require("./lib/qsa");
 
-
 //Athlete profiles
 var openBox = function(e) {
+  var box = document.querySelector('.event .show');
   var pos = this.getAttribute("data-id");
   var parent = this.parentNode;
+  var others = qsa('.show');
   var rows = qsa('.event', parent);
+  var otherRows = qsa('.event', parent);
   var items = qsa(`.athlete-box[data-id="${pos}"]`);
-  rows.forEach(function(row){
 
-    row.classList.toggle("show");
-  });
   items.forEach(function(item) {
-    item.classList.toggle("show");
-    item.innerHTML = template({athletes: window.olympians, id: pos});
+
+    if (item.classList.contains("show")) {
+      item.classList.remove("show");
+    }
+
+    else {
+      others.forEach(function(i) {
+        i.classList.remove("show");
+      });
+
+      item.classList.add("show");
+      item.innerHTML = template({athletes: window.olympians, id: pos});
+    }
+  }); 
+
+  if (screen.width < 500) { 
+  rows.forEach(function(item) {
+
+    if (item.classList.contains("show")) {
+      item.classList.remove("show");
+    }
+
+    else {
+     
+      item.classList.add("show");
+      animate(item.parentNode);
+    }
   });
+  }
 };
 
 var iconToggle = function(e) {
   var icon = this.getElementsByTagName("i")[0];
-  icon.classList.toggle("fa-minus-circle");
+  var others = qsa(".fa-minus-circle");
+
+
+
+  if (icon.classList.contains("fa-plus-circle")) {
+    icon.classList.remove("fa-plus-circle");
+    icon.classList.add("fa-minus-circle");
+  }
+
+  else if (icon.classList.contains("fa-minus-circle")) {
+
+    icon.classList.remove("fa-minus-circle");
+    icon.classList.add("fa-plus-circle");
+
+  }
+    others.forEach(function(i) {
+
+    i.classList.remove("fa-minus-circle");
+    i.classList.add("fa-plus-circle");     
+  });
+
 };
 
 qsa(".athlete-name").forEach(function(el) {
